ac5ade05cd20a62f466abadbec114cefe6a46a3b
yfktn <friskantoni@gmail.com>
2020-03-19 22:35:02 +0700

    sekarang simpan ke file ...

--- lpsescrapper.js
+++ lpsescrapper.js
@@ -1,5 +1,25 @@
+/**
+ * Ini merupakan aplikasi untuk melakukan scrapping pada halaman daftar paket dan status masing-masing paket
+ * di website LPSE, menggunakan phantomjs. Digunakannya phantomjs, karena pada daftar paket tersebut, isinya
+ * merupakan hasil render oleh javascript, sehingga dibutuhkan pembacaan dari hasil generatenya menggunakan
+ * perangkat kusus, tidak bisa langsung menggunakan fungsi di php. 
+ * Terhadap versi SPSE yang di test adalah SPSE v4.3u20191009.
+ * Hasil dari berjalannya aplikasi ini adalah dibuatnya deretan file menggunakan nama <id-paket>.htm, yang
+ * kemudian akan diproses berikutnya oleh tool lainnya. Pada prosesnya:
+ * 1. dapatkan konten dari table#dttable
+ * 2. untuk masing-masing row maka dapatkan id dan konten serta jadwal paket
+ * 3. simpan ke file bernama <id>.htm dengan isinya adalah <konten> + <jadwal>
+ * 
+ * @author Yan F (friskantoni@gmail.com)
+ */
+// url dituju
 var url = 'https://lpse.kalteng.go.id/eproc4/lelang'
+// untuk membuat file
+var fs = require('fs')
+// jumlah halaman dibaca
 var pagecnt = 0
+// maksimal halaman
+var maxpagecnt = 243
 
 /**
  * https://github.com/ariya/phantomjs/blob/master/examples/waitfor.js
@@ -37,22 +57,45 @@ function waitFor(testFx, onReady, timeOutMillis) {
         }, 250); //< repeat check every 250ms
 };
 
+/**
+ * lakukan pengambilan isi datanya.
+ */
 function scrapePage()
 {
     pagecnt = pagecnt + 1
-    console.log("Scrapping halaman: " + pagecnt)
-    // page.render(pagecnt + '.png')
-    if(pagecnt <= 10 && isNextExist(page)) {
-        clickNext(page)
-        setTimeout(function() {
-            scrapePage()
-        }, 5000)
-    } else {
-        phantom.exit()
-    }
-
+    waitFor(
+        function() {
+            // mulai proses pembacaan dan melakukan evaluasi saat halaman sudah terload
+            return page.evaluate(function () {
+                return $('div#tbllelang_paginate ul.pagination').is(':visible')
+            })
+        },
+        function() {
+            stopLoop = false // apa looping diteruskan?
+            if (pagecnt <= maxpagecnt) {
+                console.log("Scrapping page: " + pagecnt)
+                processingThePage(page, pagecnt)
+                if (isNextExist(page)) {
+                    clickNext(page)
+                    scrapePage()
+                } else {
+                    stopLoop = true
+                }
+            } else {
+                stopLoop = true
+            }
+            if( stopLoop ) {
+                phantom.exit()
+            }
+        }
+    )
 }
 
+/**
+ * Check apakah tombol untuk klik next page, tidak di disabled? Kalau di disabled artinya
+ * sudah tidak ada lagi halaman yang bisa di load
+ * @param {page} page 
+ */
 function isNextExist(page)
 {
     return page.evaluate(function() {
@@ -60,6 +103,10 @@ function isNextExist(page)
     })
 }
 
+/**
+ * click link ke halaman berikutnya
+ * @param {page} page 
+ */
 function clickNext(page)
 {
     page.evaluate(function() {
@@ -67,9 +114,43 @@ function clickNext(page)
     })
 }
 
+/**
+ * Proses halaman yang sedang aktif saat itu!
+ * @param {page} page 
+ * @param {int} currentPage 
+ */
+function processingThePage(page, currentPage)
+{
+    var tableContent = page.evaluate(function() {
+        var data = {}
+        // looping pada masing-masing baris di table lelang
+        $('table#tbllelang tr').each(function(index, el) {
+            var id = $(el).find("td:first").text(), // ambil id
+                content = $(el).find('td:nth-child(2)').html(), // isinya
+                schedule = $(el).find('td:nth-child(4)').html() // dan jadwal aktif
+            data[id] = content + '<p>' + schedule + '</p>' // simpan
+        })
+        // di phantomjs terdapat masalah bila kembalian adalah langsung array
+        // maka convert ke string dengan format JSON
+        return JSON.stringify(data);
+    })
+    // ambil datanya
+    var result = JSON.parse(tableContent)
+    // looping berdasarkan data tersebut
+    for(var key in result) {
+        content = result[key]
+        // setiap file memiliki nama id paket dan htm
+        fs.write( key + '.htm', content, 'w')
+    }
+}
+
+// mulai untuk membuat objek webpage punya phantomjs
 var page = require('webpage').create()
+
+// akses URL
 page.open(url, function(status) {
     if (status == 'success') {
+        // lakukan recursive
         scrapePage()
     } else {
         console.log("Tidak dapat mengakses url yang dikehendaki")
